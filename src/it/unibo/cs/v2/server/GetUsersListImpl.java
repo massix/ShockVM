291b8d8cc2786162b93875a6fb961f456de726b1
Massimo Gengarelli <gengarel@cs.unibo.it>
2011-05-17 19:14:10 +0200

    Removed wrong and (potentially) dangerous import in servlet.

--- src/it/unibo/cs/v2/server/GetUsersListImpl.java
+++ src/it/unibo/cs/v2/server/GetUsersListImpl.java
@@ -23,7 +23,6 @@ import java.util.TimerTask;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
